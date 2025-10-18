38504c5d9837cc1ec4c217d3da4b2cfa772d8962
bumble-maker <anna.sotnichenko@gmail.com>
2025-10-18 16:49:54 +0300

    HW2_two_func.py

--- Deep Learning/submission_template01.py
+++ Deep Learning/submission_template01.py
@@ -3,13 +3,17 @@ import torch
 from torch import nn
 
 def create_model():
+    # Linear layer mapping from 784 features, so it should be 784->256->16->10
+    layer1 = nn.Linear(784,256, bias = True)
+    layer2 = nn.Linear(256,16, bias = True)
+    layer3 = nn.Linear(16,10, bias = True)
     # your code here
+    NN = nn.Sequential(layer1, nn.ReLU(), layer2, nn.ReLU(), layer3)
     # return model instance (None is just a placeholder)
-
-    return None
+    return NN
 
 def count_parameters(model):
     # your code here
-    # return integer number (None is just a placeholder)
-    
-    return None
+    total = sum(p.numel() for p in model.parameters())
+    # верните количество параметров модели model
+    return total
