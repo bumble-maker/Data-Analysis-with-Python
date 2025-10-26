f8dc186467fd2aaf90e8d27c963fd476b89b32b0
bumble-maker <anna.sotnichenko@gmail.com>
2025-10-26 18:28:43 +0300

    SubmissionTemplate4.py

--- Deep Learning/submission_template04.py
+++ Deep Learning/submission_template04.py
@@ -5,11 +5,39 @@ from torch.nn import functional as F
 
 class ConvNet(nn.Module):
     def __init__(self):
-        ...
+        super().__init__()
 
-    
+        # ВАШ КОД ЗДЕСЬ
+        # определите слои сети
+
+        self.conv1 = nn.Conv2d(in_channels = 3, out_channels= 3, kernel_size=(5,5))
+        self.pool1 = nn.MaxPool2d(kernel_size=(2,2))
+        self.conv2 = nn.Conv2d(in_channels = 3, out_channels= 5, kernel_size=(3,3))
+        self.pool2 = nn.MaxPool2d(kernel_size=(2,2))
+
+        self.flatten = nn.Flatten()
+
+        self.fc1 = nn.Linear(180,100)
+        self.fc2 = nn.Linear(100,10)
+
+
     def forward(self, x):
-        ...
+        # размерность х ~ [64, 3, 32, 32]
+
+        # ВАШ КОД ЗДЕСЬ
+        # реализуйте forward pass сети
+
+        x = self.conv1(x)
+        x = self.pool1(x)
+        x = self.conv2(x)
+        x = self.pool2(x)
+
+        x = self.flatten(x)
+
+        x = self.fc1(F.relu(x))
+        x = self.fc2(F.relu(x))
+
+        return x
 
 def create_model():
     return ConvNet()
